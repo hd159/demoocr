42dbf6bf2cfc5a2ceb6dcb3839a99a76e3eb77a7
hd159 <81416203+hd159@users.noreply.github.com>
2023-09-20 14:07:26 +0000

    update code

--- app.py
+++ app.py
@@ -5,27 +5,29 @@ from pdf2image import convert_from_path
 import numpy as np
 from text import textString
 import csv
+from PIL import Image, ImageEnhance
 
 def perform_ocr_on_pdf(pdf_path, languages):
-    # Convert PDF pages to images
-    images = convert_from_path(pdf_path)
+    images = convert_from_path(pdf_path, dpi=300)
     reader = easyocr.Reader(languages, gpu=True)
     ocr_text = ""
-
-    # Process each image
-    for image in images:
-        image_np = np.array(image)
-        result = reader.readtext(image_np)
-        text = ' '.join([res[1] for res in result])
-        ocr_text += text + "\n"
+    for i, image in enumerate(images):
+        if i < 4:
+            image_np = np.array(image)
+            result = reader.readtext(image_np)
+            text = ' '.join([res[1] for res in result])
+            ocr_text += text + "\n"
 
     return ocr_text
 
+def enhance_image_quality(image):
+    enhancer = ImageEnhance.Sharpness(image)
+    enhanced_image = enhancer.enhance(3.0)  
+    return enhanced_image
+
 def extractText(ocr_text):
     ocr_text_lower = ocr_text.lower()
-
     text_splits = ocr_text_lower.split('correspondence information')
-
     inventors =  text_splits[0]
     inventor_pattern = r"given name middle name family name suffix(.*?)(prefix|inventors must be listed)"
     inventor_matches = re.findall(inventor_pattern, inventors, re.DOTALL)
@@ -42,37 +44,43 @@ def extractText(ocr_text):
 def extract_inventor(string):
     extract_string = re.split(r'mailing address (?:0f|of) inventor|remove inventor legal name', string)
     name_part = extract_string[0]
-    second_part = extract_string[1]
+    name = ''
+    address = ''
+    city = ''
+    state_province = ''
+    postal_code = ''
+    country = ''
+    if len(extract_string) > 1:
+        second_part = extract_string[1]
+        # Extract name
+        name_pattern = r'^(.*?) residence information'
+        name_match = re.search(name_pattern, name_part)
+        name = name_match.group(1).strip() if name_match else ''
 
-  # Extract name
-    name_pattern = r'^(.*?) residence information'
-    name_match = re.search(name_pattern, name_part)
-    name = name_match.group(1).strip() if name_match else ''
+        # Extract address
+        address_pattern = r'address (.*?) address 2'
+        address_match = re.search(address_pattern, second_part)
+        address = address_match.group(1).strip() if address_match else ''
 
-    # Extract address
-    address_pattern = r'address (.*?) address 2'
-    address_match = re.search(address_pattern, second_part)
-    address = address_match.group(1).strip() if address_match else ''
+        # Extract city
+        city_pattern = r'city (.*?) statelprovince'
+        city_match = re.search(city_pattern, second_part)
+        city = city_match.group(1).strip() if city_match else ''
 
-    # Extract city
-    city_pattern = r'city (.*?) statelprovince'
-    city_match = re.search(city_pattern, second_part)
-    city = city_match.group(1).strip() if city_match else ''
+        # Extract state/province
+        state_province_pattern = r'statelprovince (.*?) postal code'
+        state_province_match = re.search(state_province_pattern, second_part)
+        state_province = state_province_match.group(1).strip() if state_province_match else ''
 
-    # Extract state/province
-    state_province_pattern = r'statelprovince (.*?) postal code'
-    state_province_match = re.search(state_province_pattern, second_part)
-    state_province = state_province_match.group(1).strip() if state_province_match else ''
+        # Extract postal code
+        postal_code_pattern = r'postal code (.*?) countryi'
+        postal_code_match = re.search(postal_code_pattern, second_part)
+        postal_code = postal_code_match.group(1).strip() if postal_code_match else ''
 
-    # Extract postal code
-    postal_code_pattern = r'postal code (.*?) countryi'
-    postal_code_match = re.search(postal_code_pattern, second_part)
-    postal_code = postal_code_match.group(1).strip() if postal_code_match else ''
-
-    # Extract country
-    country_pattern = r'countryi (.*)'
-    country_match = re.search(country_pattern, second_part)
-    country = country_match.group(1).strip() if country_match else ''
+        # Extract country
+        country_pattern = r'countryi (.*)'
+        country_match = re.search(country_pattern, second_part)
+        country = country_match.group(1).strip() if country_match else ''
 
     inventor_dict = {
         'name': name,
